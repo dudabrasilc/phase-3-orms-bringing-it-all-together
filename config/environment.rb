d63e0eaadf77fd15c93e77fcfc2cedf6a2a2ea7f
Maria Coitinho <brasildu@gmail.com>
2022-11-14 16:27:24 -0600

    commit tree 22635124230c8f7c9aa20ed3e046405fec5f4f1f

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,11 @@
+require 'pry'
 require 'bundler'
 Bundler.require
 
 require_relative '../lib/dog'
 
 DB = { conn: SQLite3::Database.new("db/dogs.db") }
+
+dog = Dog.new(name: 'Biju', breed: 'Poodle')
+
+binding.pry
